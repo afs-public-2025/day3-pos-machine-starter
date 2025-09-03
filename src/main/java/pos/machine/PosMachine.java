fba87d1561bf20d819077bfba8bfe51e2b03ad04
AYANAMI <2206060753@qq.com>
2025-09-03 21:07:34 +0800

    [receipt]refactor:PosMachineServe

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -8,69 +8,10 @@ import java.util.stream.Collectors;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
-        Map<String, ReceiptItem> receiptItemMap = getReceiptItemMapByBarcodes(barcodes);
-        String s = buildReceipt(receiptItemMap);
+        PosMachineService posMachineService = new PosMachineService();
+        Map<String, ReceiptItem> receiptItemMap = posMachineService.getReceiptItemMapByBarcodes(barcodes);
+        String s = posMachineService.buildReceipt(receiptItemMap);
         System.out.println(s);
         return s;
-    }
-    public Map<String, ReceiptItem> getReceiptItemMapByBarcodes(List<String> barcodes){
-        Map<String, Item> itemMap = getItemMap(barcodes);
-        isItemExist(itemMap,barcodes);
-        return buildReceiptItemMap(itemMap, barcodes);
-    }
-    public Map<String, Item> getItemMap(List<String> barcodes){
-        List<Item> items = ItemsLoader.loadAllItems();
-        Map<String, Item> itemMap = new HashMap<>();
-        for (Item item : items) {
-            itemMap.put(item.getBarcode(), item);
-        }
-        return itemMap;
-    }
-    public void isItemExist(Map<String, Item> itemMap,List<String> barcodes){
-        for (String barcode : barcodes) {
-            if (!itemMap.containsKey(barcode)) {
-                throw new RuntimeException();
-            }
-        }
-    }
-    public Map<String,ReceiptItem> buildReceiptItemMap(Map<String, Item> itemMap,List<String> barcodes){
-        Map<String, ReceiptItem> receiptItemMap = new HashMap<>();
-        for (String barcode : barcodes) {
-            Item item = itemMap.get(barcode);
-            ReceiptItem receiptItem = receiptItemMap.get(item.getName());
-            if (receiptItem!=null){
-                receiptItem.setQuantity(receiptItem.getQuantity()+1);
-                receiptItemMap.put(item.getName(),receiptItem);
-            } else {
-                ReceiptItem newReceiptItem = new ReceiptItem(item,1);
-                receiptItemMap.put(item.getName(),newReceiptItem);
-            }
-        }
-        return receiptItemMap;
-    }
-
-
-    public String buildReceipt(Map<String,ReceiptItem> receiptItemMap){
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("***<store earning no money>Receipt***\n");
-        stringBuilder.append(printReceiptEntry(receiptItemMap));
-        stringBuilder.append("----------------------\n");
-        stringBuilder.append(calculateTotalAmount(receiptItemMap));
-        stringBuilder.append("**********************");
-        return stringBuilder.toString();
-    }
-    public String printReceiptEntry(Map<String,ReceiptItem> receiptItemMap){
-        return receiptItemMap.values().stream()
-                .map(item -> {
-                    return String.format(
-                            "Name: %s, Quantity: %d, Unit price: %d (yuan), Subtotal: %d (yuan)\n",
-                            item.getName(), item.getQuantity(), item.getUnitPrice(), item.getSubtotal());
-                }).collect(Collectors.joining(""));
-    }
-    public String calculateTotalAmount(Map<String,ReceiptItem> receiptItemMap){
-        int totalAmount = receiptItemMap.values().stream()
-                .mapToInt(ReceiptItem::getSubtotal)
-                .sum();
-        return String.format("Total: %d (yuan)\n", totalAmount);
     }
 }
