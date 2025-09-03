c09f9c75fdf173c2ac355b96c6f0622b95eee3a2
ZOUEL2 <zouel2@oocl.com>
2025-09-03 19:25:14 +0800

    feat:load items from database

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
+        // 加载所有商品
+        List<Item> allItems = ItemsLoader.loadAllItems();
+
         return null;
     }
+
+
 }
