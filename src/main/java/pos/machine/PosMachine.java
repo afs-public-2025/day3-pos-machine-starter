15020e178f21bc430a055b82f51b19a72cb554e1
leo.meng <1336839379@qq.com>
2025-09-03 20:40:01 +0800

    feat: find items by barcode set, throw exception if any missing

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,19 +1,33 @@
 package pos.machine;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
     }
-
+    private List<Item> allItems = ItemsLoader.loadAllItems();
     private Map<String,Integer> countItemQuantity(List<String> barcodes){
         Map<String, Integer> quantityMap = new HashMap<>();
         for (String barcode : barcodes) {
             quantityMap.put(barcode, quantityMap.getOrDefault(barcode, 0) + 1);
         }
         return quantityMap;
+    }
+    private List<Item> findItemsByBarcodes(Set<String> barcodes){
+        Map<String, Item> itemMap = new HashMap<>();
+        for (Item item : allItems) {
+            itemMap.put(item.getBarcode(), item);
+        }
+        List<Item> items = new ArrayList<>();
+        for (String barcode : barcodes) {
+            Item item = itemMap.get(barcode);
+            if (item == null) {
+                throw new RuntimeException("The following barcodes do not exist: " + barcode);
+            }
+            items.add(item);
+        }
+        return items;
     }
 }
