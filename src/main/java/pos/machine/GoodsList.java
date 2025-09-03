daaddba55dad20e964b3761466c0f35e5cfa34a5
Gavin Zhang <ZHANGGA7@OOCL.COM>
2025-09-03 20:59:56 +0800

    add GoodList

---
+++ src/main/java/pos/machine/GoodsList.java
@@ -0,0 +1,10 @@
+package pos.machine;
+
+public class GoodsList {
+    private String barcode;
+    private String name;
+    private int price;
+    private int count;
+    private int subtotal;
+
+}
