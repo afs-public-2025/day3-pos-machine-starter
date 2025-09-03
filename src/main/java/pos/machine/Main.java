992f5582f9131796be0a996aa4f2be40cd2541be
Vega Feng <vega.feng@oocl.com>
2025-09-03 20:51:57 +0800

    fix(day3): update input data

--- src/main/java/pos/machine/Main.java
+++ src/main/java/pos/machine/Main.java
@@ -5,13 +5,15 @@ public class Main {
         PosMachine posMachine = new PosMachine();
         String receipt = posMachine.printReceipt(
                 java.util.Arrays.asList(
+                        "ITEM000000",
+                        "ITEM000000",
+                        "ITEM000000",
+                        "ITEM000000",
                         "ITEM000001",
                         "ITEM000001",
-                        "ITEM000001",
-                        "ITEM000003",
-                        "ITEM000005",
-                        "ITEM000005",
-                        "ITEM000005"
+                        "ITEM000004",
+                        "ITEM000004",
+                        "ITEM000004"
                 )
         );
         System.out.println(receipt);
