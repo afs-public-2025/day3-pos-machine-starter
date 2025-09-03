bb098967096727f36b5fcb285225e5580cfa0652
KelvinTang <tangke3@oocl.com>
2025-09-03 18:52:44 +0800

    feat: add createReceipt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,9 @@ import pos.machine.PosMachine;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         PosMachine PosMachine = new PosMachine();
-        System.out.println(PosMachine.getQuantities(List.of(new String[]{
+        System.out.println(PosMachine.getItemsInfo(PosMachine.getQuantities(List.of(new String[]{
                 "ITEM000000",
                 "ITEM000000",
                 "ITEM000000",
@@ -15,6 +15,6 @@ public class Main {
                 "ITEM000004",
                 "ITEM000004",
                 "ITEM000004"
-        })));
+        }))));
     }
 }
--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -17,9 +17,11 @@ public class PosMachine {
 
     public List<Item> getItemsInfo(Map<String, Integer> itemQuantity) throws Exception {
         List<Item> items = new ArrayList<>();
-        List<Item> itemInfoList = pos.machine.ItemsLoader.loadAllItems();
-        for (Map.Entry<String, Integer> entry: itemQuantity.entrySet()) {
-            items.add(getItem(entry.getKey()));
+        List<String> barcodes = new ArrayList<>(itemQuantity.keySet());
+        Collections.sort(barcodes);
+        for (String barcode: barcodes) {
+            System.out.println(barcode);
+            items.add(getItem(barcode));
         }
         return items;
     }
@@ -32,5 +34,24 @@ public class PosMachine {
             }
         }
         throw new Exception();
+    }
+
+    public String createReceipt(Map<String, Integer> itemQuantity, List<Item> items) {
+        StringBuilder receipt = new StringBuilder();
+        receipt.append("***<store earning no money>Receipt***");
+        receipt.append("----------------------");
+        for (Item item: items) {
+            receipt.append(getReceiptEntry(item, itemQuantity.get(item.getName())));
+        }
+        receipt.append("**********************");
+        return receipt.toString();
+    }
+
+    public String getReceiptEntry(Item item, int Quantity) {
+//                "Name: Coca-Cola, Quantity: 4, Unit price: 3 (yuan), Subtotal: 12 (yuan)\n" +
+//                "Name: Sprite, Quantity: 2, Unit price: 3 (yuan), Subtotal: 6 (yuan)\n" +
+//                "Name: Battery, Quantity: 3, Unit price: 2 (yuan), Subtotal: 6 (yuan)\n" +;
+        String.format("Name: %s, Quantity: %d, Unit price: %d (yuan), Subtotal: %d (yuan)\n");
+        return "";
     }
 }
