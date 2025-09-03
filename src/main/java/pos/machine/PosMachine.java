b0ec69c162739320e28aff13a53afade81733ab8
TAMNI <tamni@oocl.com>
2025-09-03 21:46:48 +0800

    add var

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,14 @@
 package pos.machine;
 
 import java.util.List;
+import java.util.*;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
+    Map<String, Integer> itemsMap = new HashMap<>();
+    ItemsLoader db = new ItemsLoader();
+    List<Item> dbItems = db.loadAllItems();
+    int total = 0;
         return null;
     }
 }
