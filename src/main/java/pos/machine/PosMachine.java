597ea3a8f076e0f66940b33fd717e96760a21a35
ZOUEL2 <zouel2@oocl.com>
2025-09-03 19:47:07 +0800

    feat:add findItemByBarcode method

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -3,6 +3,7 @@ package pos.machine;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
@@ -15,11 +16,20 @@ public class PosMachine {
         return null;
     }
 
-    public Map<String, Integer> countItems(List<String> barcodes) {
+    private Map<String, Integer> countItems(List<String> barcodes) {
         Map<String, Integer> itemCounts = new HashMap<>();
         barcodes.forEach(barcode -> itemCounts.put(barcode, itemCounts.getOrDefault(barcode, 0) + 1));
         return itemCounts;
     }
 
+    private Item findItemByBarcode(String barcode, List<Item> allItems) {
+        if (Objects.isNull(allItems)) {
+            return null;
+        }
+        return allItems.stream()
+                .filter(item -> barcode.equals(item.getBarcode()))
+                .findFirst()
+                .orElse(null);
+    }
 
 }
