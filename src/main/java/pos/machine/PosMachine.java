1abe0c6370508126460909d12db54bdc90957395
LANA.BI <LANA.BI@OOCL.COM>
2025-09-03 22:35:50 +0800

    add barcode counting function

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,25 @@
 package pos.machine;
 
+
+import java.util.*;
 import java.util.List;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
+
     }
+    private Map<String , Integer> countBarcodes(List<String> barcodes){
+        Map<String, Integer> barcodeCountMap= new HashMap<>();
+        for (String barcode : barcodes) {
+            if (barcodeCountMap.containsKey(barcode)) {
+                barcodeCountMap.put(barcode, barcodeCountMap.get(barcode) + 1);
+            } else {
+                barcodeCountMap.put(barcode, 1);
+            }
+        }
+        return barcodeCountMap;
+    }
+
 }
+
