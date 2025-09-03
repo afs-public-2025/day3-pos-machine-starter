e2afc095d3a254cd82ee653375bf166ce292d7b6
Gavin Zhang <ZHANGGA7@OOCL.COM>
2025-09-03 22:03:41 +0800

    feat(PosMachine): add countItems

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,37 @@
 package pos.machine;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
+        List<ItemList> itemLists = new ArrayList<>();
+        countItems(barcodes, itemLists);
         return null;
+    }
+
+    public void countItems(List<String> barcodes, List<ItemList> itemLists) {
+        List<Item> items = ItemsLoader.loadAllItems();
+        for (String barcode : barcodes) {
+            boolean flag = true;
+            if(!itemLists.isEmpty()) {
+                for(ItemList itemList : itemLists) {
+                    if(itemList.getBarcode().equals(barcode)) {
+                        itemList.addCount();
+                        flag = false;
+                        break;
+                    }
+                }
+            }
+            if(flag){
+                for (Item item : items) {
+                    if(item.getBarcode().equals(barcode)) {
+                        ItemList itemList = new ItemList(barcode, item.getName(), item.getPrice());
+                        itemLists.add(itemList);
+                    }
+                }
+            }
+        }
+        System.out.println(itemLists.size());
     }
 }
