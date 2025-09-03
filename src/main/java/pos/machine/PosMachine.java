f5e2ab686b097b593d0e1d0b7122a30bb57c094f
Andy Hui <chhuiwork@gmail.com>
2025-09-03 18:23:09 +0800

    feat: defines function signatures for implementing receipt printing feature

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,38 @@
 package pos.machine;
 
 import java.util.List;
+import java.util.Map;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
+    }
+
+    private Map<String, Integer> getBarcodesOccurrence(List<String> barcodes) {
+        return null;
+    }
+
+    private boolean isAllItemsExist(Map<String, Integer> barcodeOccurrence) {
+        return false;
+    }
+
+    private Item[] retrieveItemsInfo(Map<String, Integer> barcodeOccurrence) {
+        return null;
+    }
+
+    private String buildReceipt(Map<String, Integer> barcodeOccurrence, Item[] items) {
+        return null;
+    }
+
+    private String printReceiptEntry(Item item, int occurrence) {
+        return null;
+    }
+
+    private int calculateSubtotalAmount(Item item, int occurrence) {
+        return -1;
+    }
+
+    private int calculateTotalAmount(Map<String, Integer> barcodeOccurrence, Item[] items) {
+        return -1;
     }
 }
