95a0b2191d15cefc73d5352b0fce9930461d1667
YUANJA3 <yuanja3@oocl.com>
2025-09-03 21:20:52 +0800

    test:测试方法功能是否正常

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,17 +1,22 @@
 package pos.machine;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PosMachine {
     private static final Map<String,Item> db=new HashMap<>();
-
-    public static void main(String[] args) {
+    static {
         db.put("ITEM000000",new Item("ITEM000000","Coca-Cola",3));
         db.put("ITEM000001",new Item("ITEM000001","Sprite",3));
         db.put("ITEM000004",new Item("ITEM000004","Battery",2));
+    }
 
+    public static void main(String[] args) {
+        PosMachine posMachine=new PosMachine();
+
+        List<String> barcodes=new ArrayList<>(Arrays.asList("ITEM000000","ITEM000000","ITEM000000","ITEM000000","ITEM000001","ITEM000001",
+                "ITEM000004","ITEM000004","ITEM000004"));
+
+        System.out.println(posMachine.printReceipt(barcodes));
     }
     public String printReceipt(List<String> barcodes) {
         Map<String,Integer> barcodeMap=conbineSameItemAndCalQuantity(barcodes);
@@ -20,14 +25,14 @@ public class PosMachine {
     }
 
     private Map<String,Integer> conbineSameItemAndCalQuantity(List<String> barcodes){
-        Map<String,Integer> mp=new HashMap<>();
+        Map<String,Integer> mp=new LinkedHashMap<>();
         for(String barcode:barcodes){
             mp.put(barcode,mp.getOrDefault(barcode,0)+1);
         }
         return mp;
     }
     private Map<String,Item> getRecordsFromDB(Map<String,Integer> barcodeMap){
-        Map<String,Item> ans=new HashMap<>();
+        Map<String,Item> ans=new LinkedHashMap<>();
         for(String barcode:barcodeMap.keySet()){
             ans.put(barcode,getRecordFromDB(barcode));
         }
