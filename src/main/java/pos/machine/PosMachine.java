b448460badeedd3f353df65d09a8d80f6614e17e
Vega Feng <vega.feng@oocl.com>
2025-09-03 20:03:11 +0800

    feat(day3): add function

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,23 @@
 package pos.machine;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static pos.machine.ItemsLoader.loadAllItems;
+
 public class PosMachine {
-    public String printReceipt(List<String> barcodes) {
-        return null;
+
+
+    private String generateHeader() {
+        return "***<store earning no money>Receipt ***\n";
     }
+
+    private String generateFooter(int totalPrice) {
+        return String.format("----------------------\nTotal: %d (yuan)\n**********************", totalPrice);
+    }
+
+
+
+
 }
