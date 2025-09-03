f6b8bc4b347e6dfdc6d9f1e87afe46762ee0b719
Jasmine <jasmine.lin@oocl.com>
2025-09-03 19:08:32 +0800

    建立了需要用到的方法

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,28 @@
 package pos.machine;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PosMachine {
-    public String printReceipt(List<String> barcodes) {
+
+    public static String printReceipt(List<String> barcodes) {
+        Map<String, Item> itemsMap = loadItems();
+        Map<String, Integer> itemCount = countItems(barcodes);
+        return createReceipt(itemsMap, itemCount);
+    }
+
+    private static String createReceipt(Map<String, Item> itemsMap, Map<String, Integer> itemCount) {
         return null;
     }
+
+    private static Map<String, Integer> countItems(List<String> barcodes) {
+        return null;
+    }
+
+    private static Map<String, Item> loadItems() {
+        return null;
+    }
+
 }
