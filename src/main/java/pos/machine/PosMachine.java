c1d06c1ee3171896fb3c44f10ed929963a184800
leo.meng <1336839379@qq.com>
2025-09-03 19:53:03 +0800

    feat: add function to count duplicate barcode in a list and return as map

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,19 @@
 package pos.machine;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
+    }
+
+    private Map<String,Integer> countItemQuantity(List<String> barcodes){
+        Map<String, Integer> quantityMap = new HashMap<>();
+        for (String barcode : barcodes) {
+            quantityMap.put(barcode, quantityMap.getOrDefault(barcode, 0) + 1);
+        }
+        return quantityMap;
     }
 }
