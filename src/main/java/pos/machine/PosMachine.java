188af5005c76be03087a49ee6fd2267320d3c3b3
TAMNI <tamni@oocl.com>
2025-09-03 21:50:52 +0800

    Add printReceipt function

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -9,6 +9,23 @@ public class PosMachine {
     ItemsLoader db = new ItemsLoader();
     List<Item> dbItems = db.loadAllItems();
     int total = 0;
+
+    public String printReceipt(List<String> barcodes) throws Exception {
+        String receipt = "***<store earning no money>Receipt***\n";
+        try{
+            countItems(barcodes);
+        } catch (Exception e){throw e;}
+
+        for (String barcode: itemsMap.keySet()) {
+            receipt += generateReceiptPerItem(barcode, Objects.requireNonNull(dbItems.stream().filter(item -> item.getBarcode().equals(barcode)).findFirst().orElse(null)), itemsMap.get(barcode));
+        }
+
+        receipt += "----------------------\n " +
+                "Total: " + total + "(yuan)\n" +
+                "**********************";
+
+        return receipt;
+    }
         return null;
     }
 }
