a260a37d14816c513b69623de4f3e2a4a0bc9eb4
SINJE2 <SINJE2@OOCL.COM>
2025-09-03 21:26:12 +0800

    add checkAllBarcodesValid function

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,8 +1,18 @@
 package pos.machine;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PosMachine {
+    public List<Item> itemList =  ItemsLoader.loadAllItems();
+
+    public boolean checkAllBarcodesValid(List<String> barcodes) {
+        return barcodes.stream()
+                .allMatch(barcode -> itemList.stream()
+                        .anyMatch(item -> item.getBarcode().equals(barcode)));
+    }
     public String printReceipt(List<String> barcodes) {
         return null;
     }
