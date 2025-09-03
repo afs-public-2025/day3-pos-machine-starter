a01d4ba6bcdb4c66a82eef15a6a6587ba359e408
Andy Yip <andy.yip@oocl.com>
2025-09-03 17:35:44 +0800

    Feat: Added constructor for init hashmaps for barcode and item details

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,28 @@
 package pos.machine;
 
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
 
 public class PosMachine {
+    private Set<String> uniqueBarcodes = new HashSet<>();
+    private Map<String, String> barcodeToName = new HashMap<>();
+    private Map<String, Integer> barcodeToUnitPrice = new HashMap<>();
+
+    PosMachine() {
+        for (Item item: ItemsLoader.loadAllItems()) {
+            String barcode = item.getBarcode();
+            String name = item.getName();
+            int price = item.getPrice();
+            uniqueBarcodes.add(barcode);
+            barcodeToName.put(barcode, name);
+            barcodeToUnitPrice.put(barcode, price);
+        }
+    }
+
     public String printReceipt(List<String> barcodes) {
-        return null;
+
     }
 }
